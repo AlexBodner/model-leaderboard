3889f86262adb356fda37a64b266a3d15c0b3452
Alex Bodner <yodabodner@gmail.com>
2025-06-27 12:47:08 -0300

    debuggin rtm det config

--- models/object_detection/rtmdet/run.py
+++ models/object_detection/rtmdet/run.py
@@ -103,8 +103,8 @@ def run_single_model(
     #print("Config loaded:", cfg)
     #print("Original max_per_img:", cfg.RUN_PARAMETERS["max_det"].get('max_per_img', None))
 
-    #cfg.model_test_cfg["max_per_img"] = RUN_PARAMETERS["max_det"]
-    cfg.model.test_cfg.max_per_img = RUN_PARAMETERS["max_det"]
+    cfg.model_test_cfg["max_per_img"] = RUN_PARAMETERS["max_det"]
+    #cfg.model.test_cfg.max_per_img = RUN_PARAMETERS["max_det"]
     #print("Config modified:", cfg)
 
     model = init_detector(
