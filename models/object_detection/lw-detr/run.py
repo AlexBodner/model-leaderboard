4f95709226b98afcd7705a4b7e5a937a82aa72e4
Alex Bodner <yodabodner@gmail.com>
2025-07-07 15:25:00 -0300

    remove reference to model name

--- models/object_detection/lw-detr/run.py
+++ models/object_detection/lw-detr/run.py
@@ -1,35 +1,35 @@
 import argparse
+import os
 import sys
 from pathlib import Path
+from types import SimpleNamespace
 from typing import List, Optional
 
+import numpy as np
 import supervision as sv
 import torch
 import torchvision.transforms as T
+from huggingface_hub import hf_hub_download, list_repo_files
 from PIL import Image
 from supervision.metrics import F1Score, MeanAveragePrecision
-from tqdm import tqdm
-import os
-from huggingface_hub import list_repo_files, hf_hub_download
 from torchvision import transforms
-from types import SimpleNamespace
-import numpy as np
+from tqdm import tqdm
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 import multiprocessing
 
 from configs import CONFIDENCE_THRESHOLD, DATASET_DIR
+from model_configs import MODEL_CONFIGS
 from utils import (
     load_detections_dataset,
     result_json_already_exists,
     write_result_json,
-    run_shell_command
 )
-from model_configs import MODEL_CONFIGS
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "./LW-DETR/")))
-from util.utils import ModelEma, BestMetricHolder, clean_state_dict
 from util.misc import nested_tensor_from_tensor_list
+from util.utils import ModelEma
+
 from models import build_model
 
 LICENSE = "Apache-2.0"
@@ -51,84 +51,94 @@ weight_files = [f for f in all_files if f.startswith(SUBDIR) and f.endswith(".pt
 MODEL_DICT = {}
 
 for file_path in weight_files:
-    model_name = os.path.splitext(os.path.basename(file_path))[0]  # e.g., "LWDETR_tiny_60e_coco"
+    model_name = os.path.splitext(os.path.basename(file_path))[
+        0
+    ]  # e.g., "LWDETR_tiny_60e_coco"
     print(f"Model name: {model_name}")
     MODEL_DICT[model_name] = file_path
-
 
 
 TRANSFORMS = T.Compose(
     [T.Resize((RUN_PARAMETERS["imgsz"], RUN_PARAMETERS["imgsz"])), T.ToTensor()]
 )
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-default_model_parameters =   {'lr': 1e-4,
-            'lr_encoder': 1.5e-4,
-            'weight_decay': 1e-4,
-            'epochs': 12,
-            'lr_drop': 11,
-            'clip_max_norm': 0.1,
-            'lr_vit_layer_decay': 0.8,
-            'lr_component_decay': 1.0,
-            'dropout': 0,
-            'drop_path': 0,
-            'drop_mode': 'standard',
-            'drop_schedule': 'constant',
-            'cutoff_epoch': 0,
-            'position_embedding': 'sine',
-            'dim_feedforward': 2048,
-            'decoder_norm': 'LN',
-            'set_cost_class': 2.0,
-            'set_cost_bbox': 5.0,
-            'set_cost_giou': 2.0,
-            'cls_loss_coef': 2.0,
-            'bbox_loss_coef': 5.0,
-            'giou_loss_coef': 2.0,
-            'focal_alpha': 0.25,
-            'aux_loss': True,
-            'sum_group_losses': False,
-            'use_varifocal_loss': False,
-            'use_position_supervised_loss': False,
-            'ia_bce_loss': False,
-            'pretrained_encoder': None,
-            'pretrain_weights': None,
-            'pretrain_exclude_keys': None,
-            'pretrain_keys_modify_to_load': None,
-            'output_dir': 'output',
-            'checkpoint_interval': 10,
-            'seed': 42,
-            'resume': '',
-            'start_epoch': 0,
-            'ema_decay': 0.9997,
-            'num_workers': 2,
-            'device': 'cuda',
-            'world_size': 1,
-            'dist_url': 'env://',
-            'sync_bn': True,
-            'fp16_eval': False,
-            'num_queries': 300,
+default_model_parameters = {
+    "lr": 1e-4,
+    "lr_encoder": 1.5e-4,
+    "weight_decay": 1e-4,
+    "epochs": 12,
+    "lr_drop": 11,
+    "clip_max_norm": 0.1,
+    "lr_vit_layer_decay": 0.8,
+    "lr_component_decay": 1.0,
+    "dropout": 0,
+    "drop_path": 0,
+    "drop_mode": "standard",
+    "drop_schedule": "constant",
+    "cutoff_epoch": 0,
+    "position_embedding": "sine",
+    "dim_feedforward": 2048,
+    "decoder_norm": "LN",
+    "set_cost_class": 2.0,
+    "set_cost_bbox": 5.0,
+    "set_cost_giou": 2.0,
+    "cls_loss_coef": 2.0,
+    "bbox_loss_coef": 5.0,
+    "giou_loss_coef": 2.0,
+    "focal_alpha": 0.25,
+    "aux_loss": True,
+    "sum_group_losses": False,
+    "use_varifocal_loss": False,
+    "use_position_supervised_loss": False,
+    "ia_bce_loss": False,
+    "pretrained_encoder": None,
+    "pretrain_weights": None,
+    "pretrain_exclude_keys": None,
+    "pretrain_keys_modify_to_load": None,
+    "output_dir": "output",
+    "checkpoint_interval": 10,
+    "seed": 42,
+    "resume": "",
+    "start_epoch": 0,
+    "ema_decay": 0.9997,
+    "num_workers": 2,
+    "device": "cuda",
+    "world_size": 1,
+    "dist_url": "env://",
+    "sync_bn": True,
+    "fp16_eval": False,
+    "num_queries": 300,
+    # ONNX export default (unused unless --subcommand is export_model)
+    "shape": (640, 640),
+    "infer_dir": None,
+    "verbose": False,
+    "opset_version": 17,
+    "simplify": False,
+    "tensorrt": False,
+    "dry-run": False,
+}
 
-            # ONNX export default (unused unless --subcommand is export_model)
-            'shape': (640, 640),
-            'infer_dir': None,
-            'verbose': False,
-            'opset_version': 17,
-            'simplify': False,
-            'tensorrt': False,
-            'dry-run': False}
+
 def preprocess_image(image_path):
     image = Image.open(image_path).convert("RGB")
     orig_image_size = torch.tensor(image.size[::-1])
 
-    normalize = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    transform = transforms.Compose([
+    normalize = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+    transform = transforms.Compose(
+        [
             transforms.Resize([640, 640]),
             normalize,
-        ])
+        ]
+    )
     image = transform(image)
     return image, orig_image_size
+
+
 def run_single_model(
     model_id: str,
     skip_if_result_exists=False,
@@ -141,17 +151,15 @@ def run_single_model(
         return
     if dataset is None:
         dataset = load_detections_dataset(DATASET_DIR)
-    local_path =  hf_hub_download(repo_id=REPO_ID, filename=MODEL_DICT[model_id])
+    local_path = hf_hub_download(repo_id=REPO_ID, filename=MODEL_DICT[model_id])
 
     model_cfg = MODEL_CONFIGS[model_id.lower()]
     model_cfg.update(default_model_parameters)
-    cfg = SimpleNamespace(
-        **model_cfg
-        )
-    
+    cfg = SimpleNamespace(**model_cfg)
+
     model, criterion, postprocessors = build_model(cfg)
-    checkpoint = torch.load(local_path, map_location='cpu')
-    model.load_state_dict(checkpoint['model'], strict=True)
+    checkpoint = torch.load(local_path, map_location="cpu")
+    model.load_state_dict(checkpoint["model"], strict=True)
 
     model.to(DEVICE)
     model.eval()
@@ -174,13 +182,12 @@ def run_single_model(
 
         orig_image_sizes = torch.stack([orig_image_size])
         # postprocess
-        preds= postprocessors['bbox'](outputs, orig_image_sizes)
+        preds = postprocessors["bbox"](outputs, orig_image_sizes)
 
         # visualize
-        boxes = preds[0]['boxes'].cpu().numpy()
-        labels = preds[0]['labels'].cpu().numpy()
-        scores = preds[0]['scores'].cpu().numpy()
-
+        boxes = preds[0]["boxes"].cpu().numpy()
+        labels = preds[0]["labels"].cpu().numpy()
+        scores = preds[0]["scores"].cpu().numpy()
 
         class_id = np.atleast_1d(labels).astype(int)
         xyxy = np.atleast_2d(boxes)
@@ -205,7 +212,7 @@ def run_single_model(
 
     write_result_json(
         model_id=model_id,
-        model_name=model_values["name"],
+        model_name=model_id,
         model_git_url=GIT_REPO_URL,
         paper_url=PAPER_URL,
         model=model,
